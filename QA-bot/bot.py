b051334221b4461f5207732e554ee5aaedef9847
techswarn <tech.swarns@gmail.com>
2024-08-02 15:43:51 +0530

    code update

--- QA-bot/bot.py
+++ QA-bot/bot.py
@@ -31,7 +31,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 store = {}
 #Create LLM instance
 def llm_instance():
-    model_id = "google/gemma-2b-it"
+    model_id = "google/gemma-2-9b-it"
     tokenizer = AutoTokenizer.from_pretrained(model_id)
     model = AutoModelForCausalLM.from_pretrained(model_id)
     pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=512)
